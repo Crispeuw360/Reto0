68eaada4e379a6c794a0c312803329915e6dff67
Pablo <pablovelas04@gmail.com>
2025-09-18 12:59:21 +0200

    Sql

--- MySQL/Reto0.sql
+++ MySQL/Reto0.sql
@@ -1,28 +1,43 @@
 create database examen;
+use examen;
 
 create table Unit(
 id integer not null primary key,
 acronim varchar(10),
-titulo varchar (35),
-evaluacion varchar(10),
-descripcion varchar(80)
+title varchar (35),
+evaluation varchar(10),
+description varchar(80)
 );
-create table Statement(
-id integer not null primary key,
-descripcion varchar(80),
-dificultad enum('ALTA','MEDIA','BAJA'),
-disponible boolean,
-path varchar(40)
-);
-create table Unit_Statement(
-idU integer not null ,
-idS integer not null,
-foreign key (idU) references Unit(id),
-foreign key (idS) references Statement(id)
-);
-create table callForExam(
-Ecall varchar (35) not null primary key,
+create table sessionE(
+Esession varchar (35) not null primary key,
 descripcion varchar(80),
 Edate date,
 course varchar(10)
 );
+create table Statement(
+id integer not null primary key,
+description varchar(80),
+Dlevel enum('ALTA','MEDIA','BAJA'),
+available boolean,
+path varchar(40),
+Esession varchar (35) not null,
+foreign key (Esession) references sessionE(Esession)on update cascade on delete cascade
+);
+create table Unit_Statement(
+idU integer not null ,
+idS integer not null,
+foreign key (idU) references Unit(id) on update cascade on delete cascade,
+foreign key (idS) references Statement(id)on update cascade on delete cascade
+);
+
+
+INSERT INTO unit values(1,'ADTI','data access','first','Make applications linked to databases');
+
+INSERT INTO sessionE values('primera','Make a CRUD application linked to a database','2025-05-28','first');
+
+INSERT INTO statement values(1,'Make a CRUD application linked to a database','MEDIA',true,'nowhere','primera');
+
+INSERT INTO unit_statement values(1,1);
+
+
+
